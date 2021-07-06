f784064c4806ae0e23fa1457ed8ce7f4e5e352d6
Jack Baltzer <jabb@tec.dk>
2021-07-06 08:55:23 +0200

    Added AuthorController

---
+++ LibraryProject.API/Controllers/AuthorController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryProject.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok("Hello World");
+        }
+    }
+}
