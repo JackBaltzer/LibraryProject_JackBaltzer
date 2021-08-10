683f2ebbb47601fc516c4d464cee88881efb67f8
Jack Baltzer <jack.github.com@baltzer.eu>
2021-08-10 14:10:22 +0200

    JasonWatmore fixed role

--- LibraryProject.API/Authorization/AuthorizeAttribute .cs
+++ LibraryProject.API/Authorization/AuthorizeAttribute .cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using LibraryProject.API.Database.Entities;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using System;
